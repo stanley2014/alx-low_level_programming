1a4943670908225ad65db47c7107bab05b9a58f3
stanley2014 <ogbuostanley@gmail.com>
2023-09-09 10:04:33 -0700

    5 printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
  #include <stdio.h>
 
-int main()
-{
-    printf("This is a piece of art, but the outcome is a masterpiece.\n");
+/**
+ * main - this is the main function 
+ *
+ * Return: always return 0
+ */
 
-    return 0;
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
