b854412900128a6d5c9a5474ae7d432c774bab1b
stanley2014 <ogbuostanley@gmail.com>
2023-09-09 08:55:57 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - this is the main function 
+ *
+ * Return: always return 0
+ */
+
 int main()
 {
 	puts("Programming is like building a multilingual puzzle");
-
 	return 0;
 }
