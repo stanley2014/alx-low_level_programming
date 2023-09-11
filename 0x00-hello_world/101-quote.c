563672fc42a9a7e754ddedde1c4dc7915e311115
stanley2014 <ogbuostanley@gmail.com>
2023-09-11 04:25:34 -0700

    101 quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints and that piece of art is useful - Dora Korpar, "2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: always return 1
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful"\" - Dora Korpar, 2015-10-19"\n);
+
+	return (1);
+}
