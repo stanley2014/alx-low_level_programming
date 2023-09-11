89bec5b8c54793941f83d9f716799a0e2bde56df
stanley2014 <ogbuostanley@gmail.com>
2023-09-11 11:58:49 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,11 @@
  * Return: always return 0
  */
 
-int main (void)
+int main(void)
 {
 	char letter;
 
-	for(letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
 	putchar('\n');
