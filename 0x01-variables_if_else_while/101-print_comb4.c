49bf6b5696aa2946587b61d837e8f4c0d65d9a60
stanley2014 <ogbuostanley@gmail.com>
2023-09-11 21:49:39 -0700

    tripple combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int digit1, digit2, digit3;
 
-	for (digit1 = 0; digit2 < 7; digit2++)
+	for (digit1 = 0; digit2 < 8; digit2++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 8; digit2++)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			for (digit3 = digit2 + 1; digit2 < 9; digit3++)
+			for (digit3 = digit2 + 1; digit2 < 10; digit3++)
 			{
 				putchar((digit1 % 10) + '0');
 				putchar((digit2 % 10) + '0');
@@ -26,9 +26,7 @@ int main(void)
 				putchar(',');
 				putchar(' ');
 			}
-		
 		}
-
 	}
 
 	putchar('\n');
