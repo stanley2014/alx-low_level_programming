218fbf007ade2eb00ef685129215606d3b88f449
stanley2014 <ogbuostanley@gmail.com>
2023-09-12 20:47:02 -0700

    make the alphabet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Make the alphabet
+ *
+ * Return: 1 for lowercase character or 0 for anything else
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	return (0);
+}
