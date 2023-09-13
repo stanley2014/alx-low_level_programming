52ca52c8b82d3b34cd9cbbdf0c1ac97cb64aee4d
stanley2014 <ogbuostanley@gmail.com>
2023-09-12 20:03:18 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-    char message[] = "_putchar\n";
-    write(1, message, sizeof(message) - 1);
-    return 0;
+    _putchar("_putchar");
+    _putchar('\n');
+
+    return (0);
 }
