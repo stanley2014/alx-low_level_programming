c7f0c6b39fb35b2827aab797a9fca77c9c7f7a10
stanley2014 <ogbuostanley@gmail.com>
2023-09-12 19:47:35 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * putchar -  prints _putchar, followed by a new line.
+ *
+ * Return: Always 0
+ */
+int main() {
+    char message[] = "_putchar\n";
+    write(1, message, sizeof(message) - 1);
+    return 0;
+}
