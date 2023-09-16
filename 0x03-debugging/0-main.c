c87d35ea2655a4a99eb5d0e2abb6f51418959274
stanley2014 <ogbuostanley@gmail.com>
2023-09-16 11:32:46 +0100

    yes1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,3 @@
-
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -9,10 +7,10 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 0;
-        positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
 } 
