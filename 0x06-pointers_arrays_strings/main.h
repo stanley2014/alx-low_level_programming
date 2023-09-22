6c04b4db1bd2c50f7b8a0d5c68d39363575890bd
stanley2014 <ogbuostanley@gmail.com>
2023-09-23 00:54:49 +0100

    0 strcat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
 #ifndef MYHEADER_H
 #define MYHEADER_H
 
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void);
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -10,6 +15,6 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 
-int _putchar(char c)
+int _putchar(char c);
 
 #endif /* MYHEADER_H */
