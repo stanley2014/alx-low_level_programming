4331d85daf24ee2ffd07d528311eecf8928b8528
stanley2014 <ogbuostanley@gmail.com>
2023-09-28 10:23:13 +0100

    morenum

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,13 @@ void print_most_numbers(void)
 {
         int i;
 
-        for (i = 0; i < 10; i++)
-        {
-                if (i != 2 && i != 4)
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
                 {
-                        _putchar(i + '0');
-                }
-        }
+			_putchar(i + '0');
+		 }
+	 }
 
-        _putchar('\n');
+	_putchar('\n');
 }
