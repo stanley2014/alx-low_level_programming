28a2e7e2f0f8b7b74db5e45a3ee2b8c6c7b64487
stanley2014 <ogbuostanley@gmail.com>
2023-10-06 12:29:42 +0100

    agrcagrv

---
+++ 0x0A-argc_agrv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+ * main - wrute a program that prints its name.
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	for(agrc >= 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	
+	return 0;
+}
