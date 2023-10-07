befbf9b094a1b5837ab2ef5cea493f0f438c4bb1
stanley2014 <ogbuostanley@gmail.com>
2023-10-07 21:43:21 +0100

    ddid

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,16 +3,16 @@
 #include "main.h"
 
 /*
- * main - wrute a program that prints its name.
+ * main - write a program that prints its name.
  * @argc: number of arguments
  * @argv: array of arguments
  *
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 
 	printf("%s\n", argv[argc - 1]);
 	
-	return 0;
+	return (0);
 }
