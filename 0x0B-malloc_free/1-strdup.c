0829deda162e52be8967c0fdf98fd1751176772a
stanley2014 <ogbuostanley@gmail.com>
2023-10-10 02:29:02 +0100

    rlocate

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -22,12 +22,12 @@ char *_strdup(char *str)
 	while (str[len])
 		len++;
 
-	dup = malloc(sizeof(char) * (len + 1);
+	dup = malloc(sizeof(char) * (len + 1));
 	
 	if (dup == NULL)
 		return (NULL);
 	
-	while ((dup[i] = str[i] != '\0')
+	while ((dup[i] = str[i]) != '\0')
 		i++;
 	
 	return (dup);
