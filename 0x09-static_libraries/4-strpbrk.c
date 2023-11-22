07d68838d42a8bc2c422a6888d919f1e3c7aed25
stanley2014 <ogbuostanley@gmail.com>
2023-11-23 00:52:34 +0100

    static lib

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,29 @@
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strpbrk - searches a string for any of a set of bytes
+ * @s: string to search
+ * @accept: string containing the bytes to look for
+ *
+ * Return: pointer to the byte in s that matches one of the bytes in accept
+ * or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+        int j;
+
+        for (; *s != '\0'; s++)
+        {
+                for (j = 0; accept[j] != '\0'; j++)
+                {
+                        if (*s == accept[j])
+                        {
+                                return (s);
+                        }
+                }
+        }
+
+        return (NULL);
+}
