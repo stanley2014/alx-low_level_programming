ce67877f3175e9dc24884453afd556a87efbee59
stanley2014 <ogbuostanley@gmail.com>
2023-11-27 14:13:21 +0100

    latest

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,9 +3,9 @@
 /** main.c - program that prints the name of file it was comiled from
  *
  * Return 0
- */
+*/
 
 main (void)
 {
-	printf("File: %s\n", __FILE__);
+	printf("File: %s\n", __FILE__\n);
 }
