22010a6638d749eccf2e178a04c8310f0692f3dc
stanley2014 <ogbuostanley@gmail.com>
2023-11-29 06:33:18 +0100

    lol

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -6,17 +6,13 @@
  * @name - name of dog
  * @owner- owner of dog
  * d - pointer to dog_t
- *
- * Return: Always 0
  */
 void free_dog(dog_t *d)
 {
 	if(d != NULL)
 	{
-		free(d -> name);
-		free(d -> owner);
+		free(d->name);
+		free(d->owner);
 		free(d);
 	}
-
-	return (0);
 }
