0e2a7738f56232ec1e8d467c6c875edc8e9cf0ba
stanley2014 <ogbuostanley@gmail.com>
2023-11-29 05:56:01 +0100

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,23 @@
+#include <stdlib>
+#include "dog.h"
+
+/**
+ * free dog - frees the memory allocated for dog_t
+ * @name - name of dog
+ * @owner- owner of dog
+ * d - pointer to dog_t
+ *
+ * Return: Always 0
+ */
+void free_dog(dog_t *d)
+{
+	if(d != NULL)
+	{
+		free(d -> name);
+		free(d -> age);
+		free(d -> owner);
+		free(d);
+	}
+
+	return(0);
+}
