1ee79725a652748f7d53de5d20cfc8a759075e2f
stanley2014 <ogbuostanley@gmail.com>
2023-11-30 13:25:16 +0100

    rellly

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,7 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	{
 		for (n = 0; n < size; n++)
-			if cmp(array[n] != 0)
+			if (cmp(array[n]) != 0)
 			{
 				return n;
 			}
