3e3fb837771bc69b8e06b8c573baa3c569def52d
stanley2014 <ogbuostanley@gmail.com>
2023-12-01 01:33:30 +0100

    varrry

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -18,14 +18,12 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(sp, n);
 
-	if (n == 0)
-		return 0;
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-			sum += var_arg(sp, int);
-		
-		va_end(list);
+		sum += var_arg(sp, int);
 	}
+
+	va_end(sp);
 
 	return (sum);
 }
