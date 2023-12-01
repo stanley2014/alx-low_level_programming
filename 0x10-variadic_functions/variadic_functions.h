a20cc4f06789d63594199a39e4f2e1b387f9333b
stanley2014 <ogbuostanley@gmail.com>
2023-12-01 02:54:02 +0100

    lies

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef VARIADICFUNCTIONS_H
+#define VARIADICFUNCTIONS_H
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
@@ -7,4 +7,4 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
 
-#endif /*VARIADIC_FUNCTIONS_H*/
+#endif /*VARIADICFUNCTIONS_H*/
