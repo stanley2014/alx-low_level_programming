420acac2d990598adedf3411e4eb11edfe60da55
stanley2014 <ogbuostanley@gmail.com>
2023-12-02 22:40:16 +0100

    ber

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,11 +3,11 @@
 #include "function_pointers"
 
 /**
- * @array: array to iterate through
- * @size: size of the array
- * @action: pointer to the function used
  * array_iterator - this executes a function given as a parameter on
  * each element of an array.
+ * @array: array to iterate over
+ * @size: size of the array
+ * @action: pointer to the function used
  *
  * Return: Nothing
  */
